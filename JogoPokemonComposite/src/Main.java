ab30e87ae2f99f415e285e3f890f936d0582401f
Kaio Silveira <skaiocesar4960@gmail.com>
2025-03-19 19:21:31 -0300

    Arquivos.class

--- JogoPokemonComposite/src/Main.java
+++ JogoPokemonComposite/src/Main.java
@@ -1,11 +1,11 @@
 public class Main {
     public static void main(String[] args) throws Exception {
-        CartaPokemon bulbasaur = new PokemonSimples("Bulbasaur");
+        CartaPokemon bulbasaur = new PokemonSimples("Bulbasauro");
         CartaPokemon charmander = new PokemonSimples("Charmander");
 
         // Pokémon com Evolução
-        PokemonEvoluido ivysaur = new PokemonEvoluido("Ivysaur");
-        ivysaur.adicionarEvolucao(new PokemonSimples("Venusaur"));
+        PokemonEvoluido ivysaur = new PokemonEvoluido("Ivysauro");
+        ivysaur.adicionarEvolucao(new PokemonSimples("Venusauro"));
 
         PokemonEvoluido charmeleon = new PokemonEvoluido("Charmeleon");
         charmeleon.adicionarEvolucao(new PokemonSimples("Charizard"));
